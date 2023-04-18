f42509c80007b90d6fa0db54ab042a2b9b955396
mmustafayev213-3 <mustafamehmed1251@gmail.com>
2023-04-18 18:18:24 +0400

    Task Loops added

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new String('-',30));
+            string userName;
+            string password;
+            do
+            { 
+                Console.WriteLine("Zehmet olmasa istifadeci adinizi girin:");
+                userName = Console.ReadLine().Trim().ToLower();
+                Console.WriteLine("Zehmet olmasa istifadeci parolunuzu girin:");
+                password = Console.ReadLine().Trim().ToLower();
+            } while (userName != "admin" && password!="123");
+            Console.WriteLine(new String('-', 30));
+            Console.WriteLine("Xosh Geldiniz");
+        }
+    }
+}
